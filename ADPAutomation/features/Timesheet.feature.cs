7c935f1921197d57d63dfa9373e25080deebd4ef
Michael L Nacey <mnacey@gmail.com>
2015-11-05 15:29:10 -0500

    fixed

--- ADPAutomation/features/Timesheet.feature.cs
+++ ADPAutomation/features/Timesheet.feature.cs
@@ -69,7 +69,7 @@ namespace ADPAutomation.Features
         {
 #line 5
 #line 6
- testRunner.Given("I am logged in to ADP with user \" \" and password \"\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+ testRunner.Given("I am logged in to ADP with user \" \" and password \" \"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
         }
         
